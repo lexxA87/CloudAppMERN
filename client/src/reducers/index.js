b106eeb511958500a4641e7b8bc37602fc9e089f
lexxA87 <alex.o.nikonov@gmail.com>
2022-05-23 23:53:18 +0400

    4. Added redux and reducers for user and file

---
+++ client/src/reducers/index.js
@@ -0,0 +1,19 @@
+import {
+  combineReducers,
+  applyMiddleware,
+  legacy_createStore as createStore,
+} from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import fileReducer from "./fileReducer";
+import userReducer from "./userReducer";
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  file: fileReducer,
+});
+
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
